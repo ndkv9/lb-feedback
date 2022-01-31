20795dd6e374622b42eb3c6a7276c35ce4c5966c
Vu <vu.nguyen101@gmail.com>
2022-01-31 13:09:04 +0200

    Add submit btn

--- src/components/FeedBackBar.js
+++ src/components/FeedBackBar.js
@@ -4,6 +4,8 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const FeedBackBar = () => {
 
@@ -11,9 +13,29 @@ const FeedBackBar = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 0 }}>
+          <Box sx={{ flexGrow: 1 }}>
             <Avatar alt="Remy Sharp" src="https://stripe-images.s3.us-west-1.amazonaws.com/works-with/0bf92fa8419e3237a6fb6eac9b1c225099f96741" />
           </Box>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            marginRight="50px"
+          >
+            <div>
+              <TextField
+                id="standard-textarea"
+                label="Feedback"
+                placeholder="Placeholder"
+                multiline
+                variant="standard"
+              />
+            </div>
+          </Box>
+          <Button variant="contained">Submit</Button>
         </Toolbar>
       </Container>
     </AppBar>
